33d53f6e5af02b98d30f34e5dd8eea1f2e920bc3
Rostislav Statko <rostislav.statko@gmail.com>
2023-02-01 22:30:47 +0300

    nice

--- Lab2_1/Form1.cs
+++ Lab2_1/Form1.cs
@@ -17,8 +17,6 @@ namespace Lab2_1
 {
     public partial class Server : Form
     {
-        StreamReader sr;
-        StreamWriter sw;
         TcpListener server;
         int port;
         IPAddress ipAddress;
@@ -41,19 +39,19 @@ namespace Lab2_1
 
         private void textPort_TextChanged(object sender, EventArgs e)
         {
-            
+
         }
 
         private void buttonStop_Click(object sender, EventArgs e)
         {
 
             server.Server.Close();
-            textStatus.Text += "Server stoped " + Environment.NewLine; 
+            textStatus.Text += "Server stoped " + Environment.NewLine;
         }
 
         private async void button1_Click(object sender, EventArgs e)
         {
-
+            var directory = Directory.GetCurrentDirectory() + "\\exe";
             ipAddress = IPAddress.Parse(textHost.Text);
             port = int.Parse(textPort.Text);
             // TcpListener server = new TcpListener(port);
@@ -65,71 +63,66 @@ namespace Lab2_1
             //Enter the listening loop.
             while (true)
             {
-                TcpClient client = null;
                 textStatus.Text += "Waiting for a connection... " + Environment.NewLine;
                 try
                 {
-                    client = await server.AcceptTcpClientAsync();
-                } catch (Exception ex)
-                {
-                    textStatus.Text += "Server shutdown... " + Environment.NewLine;
-                    break;
-                }
-
-
-                BinaryWriter bw = null;
-                if (client != null && client.Connected) {
-                    textStatus.Text += "Connected!" + Environment.NewLine;
-                    try
+                    using (TcpClient client = await server.AcceptTcpClientAsync())
+                    using (StreamReader sr = new StreamReader(client.GetStream()))
+                    using (StreamWriter sw = new StreamWriter(client.GetStream()))
                     {
-                        sr = new StreamReader(client.GetStream());
-                        sw = new StreamWriter(client.GetStream());
                         sw.AutoFlush = true;
+                        textStatus.Text += "Connected!" + Environment.NewLine;
                         // Get a stream object for reading and writing
                         // Buffer for reading data
 
-                        string data = await sr.ReadLineAsync();
-
-                        textStatus.Text += "Read client request: " + data + Environment.NewLine;
-
-                        string directory = Directory.GetCurrentDirectory() + "\\exe";
-                        string[] files = Directory.GetFiles(directory);
-                        string all = "";
-                        for (int j = 0; j < files.Length; j++)
+                        while (client.Connected)
                         {
-                            if (files[j].EndsWith(".exe"))
+                            var serverMethod = await sr.ReadLineAsync();
+                            textStatus.Text += "Read client request: " + serverMethod + Environment.NewLine;
+
+                            switch (serverMethod)
                             {
-                                int indexDelimeter = files[j].LastIndexOf('\u005C');
-                                textStatus.Text += "Sending file " + files[j].Substring(indexDelimeter + 1) + Environment.NewLine;
-                                all += files[j].Substring(indexDelimeter + 1) + ";";
+                                case "listFiles":
+                                    var availableExeFiles = "";
+                                    foreach (var file in Directory.GetFiles(directory))
+                                    {
+                                        if (file.EndsWith(".exe"))
+                                        {
+                                            int indexDelimeter = file.LastIndexOf('\u005C');
+                                            textStatus.Text += "Sending file " + file.Substring(indexDelimeter + 1) +
+                                                               Environment.NewLine;
+                                            availableExeFiles += file.Substring(indexDelimeter + 1) + ";";
+                                        }
+                                    }
+                                    await sw.WriteLineAsync(availableExeFiles);
+                                    break;
+                                case "sendFile":
+                                    textStatus.Text += "Sending files " + Environment.NewLine;
+                                    var currentFile = await sr.ReadLineAsync();
+
+                                    var sendingFile = directory + "\\" + currentFile;
+                                    textStatus.Text += "Reading file into bytes " + currentFile + Environment.NewLine;
+
+                                    var fileInBytes = File.ReadAllBytes(sendingFile);
+                                    using (BinaryWriter bw = new BinaryWriter(client.GetStream(),  new UTF8Encoding(false, true), leaveOpen: true))
+                                    {
+                                        bw.Write(fileInBytes);
+                                    }
+                                    
+                                    textStatus.Text += "Sending file for execution " + currentFile + Environment.NewLine;
+                                    break;
+                                default:
+                                    textStatus.Text += "Forbidden method " + Environment.NewLine;
+                                    break;
                             }
-
                         }
-                        all.Substring(0, all.Length - 1);
-                        await sw.WriteLineAsync(all);
-                        textStatus.Text += "Sending files " + Environment.NewLine;
-                        String currentFile = await sr.ReadLineAsync();
-                        
-                        String sendingFile = directory + "\\" + currentFile;
-                        textStatus.Text += "Reading file into bytes " + currentFile + Environment.NewLine;
-
-                        byte[] fileInBytes = File.ReadAllBytes(sendingFile);
-                        bw = new BinaryWriter(client.GetStream());
-
-                        bw.Write(fileInBytes);
-                        textStatus.Text += "Sending file for execution " + currentFile + Environment.NewLine;
-
-                    }
-                    finally
-                    {
-                        bw.Close();
-                        sr.Close();
-                        sw.Close();
                     }
                 }
-                
+                catch (Exception exception)
+                {
+                    textStatus.Text += $"Socket ex: {exception.Message} " + Environment.NewLine;
+                }
             }
         }
-
     }
 }
